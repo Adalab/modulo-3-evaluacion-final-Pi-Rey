feee9537df8485778b1d041c9df0c67c410caf2c
Pi Rey <pilar.rrdgz@gmail.com>
2024-05-23 16:44:49 +0200

    add death

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -2,16 +2,20 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
 function Character({ character }) {
+    const hidden = character.status !== "Dead" ? "hidden" : "";
   return (
     <article className="article">
       <Link to={"/character/" + character.id} className="article--link">
-        <h3 className="article--h3" >{character.name}</h3>
+        <h3 className="article--h3" >{character.name}
+        <span className={`article--span ${hidden}`}> 💀</span>
+        </h3>
         <img
           className="article--img"
           src={character.img}
           alt={character.name}
         />
         <p className="article--p" >{character.species}</p>
+
       </Link>
     </article>
   );
